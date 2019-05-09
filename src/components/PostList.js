89d6e5870f2d63f1e99221ebc21a6166667f4468
Luis <luchopintado@gmail.com>
2019-05-09 17:18:43 -0500

    finish postlist

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends Component {
     componentDidMount() {
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
 
     renderList () {
@@ -15,8 +16,9 @@ class PostList extends Component {
                     <div className="content">
                         <div className="description">
                             <h2>{post.title}</h2>
-                            <h2>{post.body}</h2>
+                            <p>{post.body}</p>
                         </div>
+                        <UserHeader userId={post.userId} />
                     </div>
                 </div>
             );
@@ -36,4 +38,4 @@ const mapStateToProps = (state) => {
     return { posts: state.posts };
 };
 
-export default connect(mapStateToProps, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList);
