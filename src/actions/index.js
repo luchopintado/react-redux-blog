c1720608acffe20e42c2c72ecbc6a4137d705856
Luis <luchopintado@gmail.com>
2019-05-09 17:18:33 -0500

    refactor actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,26 @@
 import jsonPlaceholder from '../apis/jsonPlacehoder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    new Set(getState().posts.map(post => post.userId))
+    .forEach(userId => dispatch(fetchUser(userId)));
+};
+
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({
         type: 'FETCH_POSTS',
+        payload: response.data,
+    });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({
+        type: 'FETCH_USER',
         payload: response.data,
     });
 };
